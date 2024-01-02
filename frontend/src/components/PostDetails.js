ea63186b3dc9a2e6fbc3d5e818dbcb14b9e654a3
Niladri Das <niladri.das@lpu.in>
2024-01-02 22:10:25 +0530

    Create PostDetails.js

---
+++ frontend/src/components/PostDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PostDetails() {
+    // Fetch and display details of a specific blog post
+    return (
+        <div>
+            <h2>Post Title</h2>
+            {/* Display post details here */}
+        </div>
+    );
+}
+
+export default PostDetails;
