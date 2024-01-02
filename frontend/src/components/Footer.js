e4d107e305e7a76afa645db719067060c58b1e16
Niladri Das <niladri.das@lpu.in>
2024-01-02 22:09:42 +0530

    Create Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            {/* Add footer content, copyright, etc. */}
+            <p>&copy; 2024 UnityClubXtra. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
