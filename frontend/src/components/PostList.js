94489734e0b6957356e9428141a24120e16fdb86
Niladri Das <niladri.das@lpu.in>
2024-01-02 22:10:04 +0530

    Create PostList.js

---
+++ frontend/src/components/PostList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PostList() {
+    // Fetch and display a list of blog posts
+    return (
+        <div>
+            <h2>Latest Posts</h2>
+            {/* Display posts here */}
+        </div>
+    );
+}
+
+export default PostList;
