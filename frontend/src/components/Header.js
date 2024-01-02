2b25dbc7226b949f86c9ebc6dc059c2dc504d61b
Niladri Das <niladri.das@lpu.in>
2024-01-02 22:09:07 +0530

    Create Header.js

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>UnityClubXtra</h1>
+            {/* Add navigation links or other header content */}
+        </header>
+    );
+}
+
+export default Header;
